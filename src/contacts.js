dcb4c7e54cd1a70443a98601405f7538d729211d
Olawuwo2000 <76418051+Olawuwo2000@users.noreply.github.com>
2022-06-13 03:04:16 +0100

    Add files via upload

---
+++ src/contacts.js
@@ -0,0 +1,29 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Abideen",
+    imgURL:
+      "images/Abideen.jpg",
+    phone: "+123 456 789",
+    email: "abideen@b.com"
+  },
+  {
+    id: 2,
+    name: "Simon",
+    imgURL:
+      "images/Simon.png",
+    phone: "+987 654 321",
+    email: "simon@nowhere.com"
+  },
+  {
+    id: 3,
+    name: "Bisola",
+    imgURL:
+      "images/Bisola.jpeg",
+    phone: "+918 372 574",
+    email: "bisola@gmail.com"
+   
+  }
+];
+
+export default contacts;
